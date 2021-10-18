573017b3f28245313af13d8e0cc8dd63df0a77ab
Jean Paul Delaye <92291163+jpdelaye@users.noreply.github.com>
2021-10-17 23:09:33 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Monocrea Class
+ * This class will output 3 sgv rectangles filled with a random color each one.
+ * @author Jean Paul Delaye 
+ */
+
+require_once('class.monocrea.php');
+
+	  $artwork = new monocrea();
+
+?>
